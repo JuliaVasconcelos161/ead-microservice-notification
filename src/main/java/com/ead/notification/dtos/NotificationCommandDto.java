6c966fe8f7d588e55429e59a8a1824753f59dccf
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-11-01 14:55:00 -0300

    feat: adiciona classe de configuração do rabbitmq e cria consumer para Notification

---
+++ src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -0,0 +1,34 @@
+package com.ead.notification.dtos;
+
+import java.util.UUID;
+
+public class NotificationCommandDto {
+
+    private String titulo;
+    private String messsage;
+    private UUID userId;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMesssage() {
+        return messsage;
+    }
+
+    public void setMesssage(String messsage) {
+        this.messsage = messsage;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+}
