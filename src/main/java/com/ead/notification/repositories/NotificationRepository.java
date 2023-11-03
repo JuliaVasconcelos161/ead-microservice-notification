8b10bf681ec5f66338a8dc2beea8286a4f11e579
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-11-02 23:35:38 -0300

    feat: cria método que retorna todos as notificações com status created por id de usuário

--- src/main/java/com/ead/notification/repositories/NotificationRepository.java
+++ src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -1,10 +1,15 @@
 package com.ead.notification.repositories;
 
+import com.ead.notification.enums.NotificationStatus;
 import com.ead.notification.models.NotificationModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
+
+    Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
 }
